952bab01a362be7c85379f3cae3b3a889f0c66d6
tokusea <k.tokusea.285120@gmail.com>
2022-07-27 22:22:58 +0900

    define models

---
+++ uber-eats-like/app/models/food.rb
@@ -0,0 +1,7 @@
+# --- ここから追加 ---
+class Food < ApplicationRecord
+  belongs_to :restaurant
+  belongs_to :order, optional: true
+  has_one :line_food
+end
+# --- ここまで追加 ---
--- uber-eats-like/config/routes.rb
+++ uber-eats-like/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # --- ここから追加 ---
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index create]
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
+  # --- ここまで追加 ---
 end
