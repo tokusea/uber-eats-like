5c5136298411100624992cf795ee1e893df53081
tokusea <k.tokusea.285120@gmail.com>
2022-08-05 09:17:55 +0900

    いろいろ

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+// apis
+import { fetchRestaurants } from '../apis/restaurants'; //←この行を追加する
 
 export const Restaurants = () => {
+
+//=====ここから追加する=====
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+//=====ここまで追加する=====
   return (
     <Fragment>
       レストラン一覧
