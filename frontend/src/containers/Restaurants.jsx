7b412747f1ed775f4655153c4627124f4fd4123d
tokusea <k.tokusea.285120@gmail.com>
2022-08-07 11:35:41 +0900

    add: mui

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,21 +1,49 @@
+// --- ここから追加 ---
 import React, { Fragment, useEffect } from 'react';
+import styled from 'styled-components';
 
 // apis
-import { fetchRestaurants } from '../apis/restaurants'; //←この行を追加する
+import { fetchRestaurants } from '../apis/restaurants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
-
-//=====ここから追加する=====
   useEffect(() => {
     fetchRestaurants()
     .then((data) =>
       console.log(data)
     )
   }, [])
-//=====ここまで追加する=====
+
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
     </Fragment>
   )
 }
+// --- ここまで追加 ---
