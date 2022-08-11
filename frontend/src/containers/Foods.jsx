e2397c872100474665028871410d2efb94119e43
tokusea <k.tokusea.285120@gmail.com>
2022-08-11 12:33:21 +0900

    feat: foodにおけるモーダルの作成

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -57,6 +57,10 @@ const FoodsList = styled.div`
 const ItemWrapper = styled.div`
   margin: 16px;
 `;
+const submitOrder = () => {
+  // 後ほど仮注文のAPIを実装します
+  console.log('登録ボタンが押された！')
+}
 
 export const Foods = ({
   match
@@ -130,11 +134,25 @@ export const Foods = ({
       {
         state.isOpenOrderDialog &&
           <FoodOrderDialog
-            food={state.selectedFood}
             isOpen={state.isOpenOrderDialog}
+            food={state.selectedFood}
+            countNumber={state.selectedFoodCount}
+            onClickCountUp={() => setState({
+              ...state,
+              selectedFoodCount: state.selectedFoodCount + 1,
+            })}
+            onClickCountDown={() => setState({
+              ...state,
+              selectedFoodCount: state.selectedFoodCount - 1,
+            })}
+            // 先ほど作った関数を渡します
+            onClickOrder={() => submitOrder()}
+            // モーダルを閉じる時はすべてのstateを初期化する
             onClose={() => setState({
               ...state,
               isOpenOrderDialog: false,
+              selectedFood: null,
+              selectedFoodCount: 1,
             })}
           />
       }
